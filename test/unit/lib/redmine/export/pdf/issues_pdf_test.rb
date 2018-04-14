29e94435b8af8e36d946a9617742abaf7d78d58e
kevinyoutha <kevin.youtha@gmail.com>
2018-04-14 14:28:33 -0400

    Reproduce issue with pdf helper test case
    Ajouté par Gregor Schmidt il y a environ un mois. Mis à jour il y a environ un mois.
    
    Statut:	Closed	Début:	
    Priorité:	Normal	Echéance:	
    Assigné à:	Go MAEDA	% réalisé:	
    0%
    
    Catégorie:	PDF export		
    Version cible:	-		
    Resolution:	Fixed	Affected version:	
    Description
    
    The IssuesPdfHelperTest test fails, when starting it in isolation. It's missing the neccessary fixture definitions. Depending on the random test order, this might also lead to errors, when running the whole test suite.

--- test/unit/lib/redmine/export/pdf/issues_pdf_test.rb
+++ test/unit/lib/redmine/export/pdf/issues_pdf_test.rb
@@ -18,8 +18,9 @@
 require File.expand_path('../../../../../../test_helper', __FILE__)
 
 class IssuesPdfHelperTest < ActiveSupport::TestCase
+  
   #fixtures :users, :projects, :roles, :members, :member_roles,
-   #        :enabled_modules, :issues, :trackers, :enumerations
+  #        :enabled_modules, :issues, :trackers, :enumerations
 
   include Redmine::Export::PDF::IssuesPdfHelper
 
