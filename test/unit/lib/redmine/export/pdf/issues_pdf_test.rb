b6f55ad2bc3595a9854e83ac2134ff28b5c28d52
kevinyoutha <kevin.youtha@gmail.com>
2018-04-14 14:49:40 -0400

    Update issues_pdf_test.rb
    Adding missing features regardly the test case implementation

--- test/unit/lib/redmine/export/pdf/issues_pdf_test.rb
+++ test/unit/lib/redmine/export/pdf/issues_pdf_test.rb
@@ -19,8 +19,8 @@ require File.expand_path('../../../../../../test_helper', __FILE__)
 
 class IssuesPdfHelperTest < ActiveSupport::TestCase
   
-  #fixtures :users, :projects, :roles, :members, :member_roles,
-  #        :enabled_modules, :issues, :trackers, :enumerations
+  fixtures :users, :projects, :roles, :members, :member_roles,
+          :enabled_modules, :issues, :enumerations
 
   include Redmine::Export::PDF::IssuesPdfHelper
 
